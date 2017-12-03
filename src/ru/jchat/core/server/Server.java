818be05dde0a525da2524f0823e77103bd8bd8c0
Ruslan <sonikx94@yandex.ru>
2017-12-03 14:10:53 +0300

    Массив подключённых клиентов

--- src/ru/jchat/core/server/Server.java
+++ src/ru/jchat/core/server/Server.java
@@ -3,15 +3,19 @@ package ru.jchat.core.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Vector;
 
 public class Server {
+    private Vector<ClientHandler> clients;
     public Server() {
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            clients = new Vector<>();
             System.out.println("Сервер запущен. Ожидаю клиентов...");
+
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Клиент подключен");
-                new ClientHandler(socket);
+                clients.add(new ClientHandler(socket));
             }
         } catch (IOException e) {
             e.printStackTrace();
